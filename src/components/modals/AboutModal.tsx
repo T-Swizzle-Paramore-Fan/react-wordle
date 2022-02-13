65cef6c2925226f6a9538eb5d130ee9cfdfe73fe
T-Swizzle-Paramore-Fan <99606530+T-Swizzle-Paramore-Fan@users.noreply.github.com>
2022-02-13 14:22:38 -0600

    Update AboutModal.tsx

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -9,8 +9,8 @@ export const AboutModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="About" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        This is an open source version of the word guessing game we all know and
-        love -{' '}
+        This is a paramore version of the word guessing game we all know and
+        love-{' '}
         <a
           href="https://github.com/cwackerfuss/react-wordle"
           className="underline font-bold"
